f20a1d712e2f056d2e87137cee598c37f73ffcba
sebastian <segallar@alumnos.inf.utfsm.cl>
2017-11-05 15:57:00 -0300

    configuracion de ahorro

--- app/src/main/java/cl/usm/inf/walletkeeper/DefineBudgetActivity.java
+++ app/src/main/java/cl/usm/inf/walletkeeper/DefineBudgetActivity.java
@@ -1,16 +1,28 @@
 package cl.usm.inf.walletkeeper;
 
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.List;
+
+import cl.usm.inf.walletkeeper.adapters.CategorySeekbarAdapter;
+import cl.usm.inf.walletkeeper.db.DbHelper;
+import cl.usm.inf.walletkeeper.structs.Category;
 
 public class DefineBudgetActivity extends AppCompatActivity
         implements View.OnClickListener {
 
     private EditText BudgetEntry;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +37,16 @@ public class DefineBudgetActivity extends AppCompatActivity
         btn.setOnClickListener(this);
 
         BudgetEntry = (EditText) findViewById(R.id.entryBudget);
+
+        //Lista de seekbars
+        SQLiteDatabase db = new DbHelper(this).getWritableDatabase();
+        List<Category> categories = DbHelper.GET_CATEGORY(db);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerSaving);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new CategorySeekbarAdapter(this,categories);
+        mRecyclerView.setAdapter(mAdapter);
+        db.close();
     }
 
     @Override
